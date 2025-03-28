08d385e05173f8fedb163e18a598a636a89db66f
fr13n8 <ovsepanrik@gmail.com>
2025-03-28 14:33:33 +0400

    Add lesson 33

--- lesson_33/src/App.jsx
+++ lesson_33/src/App.jsx
@@ -1,21 +1,37 @@
+import { useState } from 'react'
 import './App.scss'
 
 function App() {
+  const [username, setUsername] = useState("")
+  const handleUsername = function (e) {
+    setUsername(e.target.value)
+  }
+
+  const [email, setEmail] = useState("")
+  const handleEmail = function (e) {
+    setEmail(e.target.value)
+  }
+
+  const [password, setPassword] = useState("")
+  const handlePassword = function (e) {
+    setPassword(e.target.value)
+  }
+
   return (
     <div className="form-block">
       <h1>Sign Up</h1>
       <form className='form'>
         <div>
-          <label htmlFor="">Username</label>
-          <input type="text" id='username' />
+          <label htmlFor="">Username {username}</label>
+          <input onChange={handleUsername} value={username} type="text" id='username' />
         </div>
         <div>
-          <label htmlFor="">Email</label>
-          <input type="email" id='email' />
+          <label htmlFor="">Email {email}</label>
+          <input onChange={handleEmail} value={email} type="email" id='email' />
         </div>
         <div>
-          <label htmlFor="">Password</label>
-          <input type="password" id='password' />
+          <label htmlFor="">Password {password}</label>
+          <input onChange={handlePassword} value={password} type="password" id='password' />
         </div>
         <button>Submit</button>
       </form>
